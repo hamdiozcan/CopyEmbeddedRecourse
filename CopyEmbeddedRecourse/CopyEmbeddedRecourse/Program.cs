b849fba94f2371624419fcb6d236bdc6f1feaa8e
Hamdi ÖZCAN <ahmethamdi@gmail.com>
2016-07-31 04:33:33 +0300

    Add files via upload

---
+++ CopyEmbeddedRecourse/CopyEmbeddedRecourse/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CopyEmbeddedRecourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var instance = new Copy();
+            instance.CopyInstance();
+        }
+}
+
+    public class Copy
+    {
+        public void CopyInstance()
+        {
+            CopyResource("CopyEmbeddedRecourse.hamdi", @"C:\hamdi.ttf");
+        }
+
+        public void CopyResource(string resourceName, string file)
+        {
+            using (Stream resource = GetType().Assembly.GetManifestResourceStream(resourceName))
+            {
+                if (resource == null)
+                {
+                    throw new ArgumentException("No such resource", "resourceName");
+                }
+                using (Stream output = File.OpenWrite(file))
+                {
+                    resource.CopyTo(output);
+                }
+            }
+        }
+
+    }
+}
